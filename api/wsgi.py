63e082677918f01c77099ecc96eb1e6d30fa8d8f
Hikkstar <54229046+Hikkstar@users.noreply.github.com>
2025-10-08 18:19:34 +0900

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,8 +1,9 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Django-master.settings')
 
 application = get_wsgi_application()
+
 
 
