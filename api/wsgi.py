be01916c53b751539c98f14ccf157c61c01e4f32
Hikkstar <54229046+Hikkstar@users.noreply.github.com>
2025-10-08 18:18:33 +0900

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -1,8 +1,8 @@
-from django.core.wsgi import get_wsgi_application
-from mangum import Mangum  # これを使ってサーバレス化
-
 import os
+from django.core.wsgi import get_wsgi_application
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
 
 application = get_wsgi_application()
-handler = Mangum(application)
+
+
